f99b0c5d2a400c7324aef6b9fe8327ccb690d6c2
Carla Holcomb <carla.programmer@gmail.com>
2020-11-11 01:14:35 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -19,7 +19,8 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+          <input type="text" onChange={this.handleChange} value={this.state.message}/>
+          {charNumber}
       </div>
     );
   }
