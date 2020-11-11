090930c4256bcea71d6f6bfb0f069c63faf64577
Carla Holcomb <carla.programmer@gmail.com>
2020-11-11 01:19:44 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -24,27 +24,27 @@ class LoginForm extends React.Component {
   this.props.handleLogin(this.state)
 }
 
-  render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
-  }
+render() {
+   return (
+     <form onSubmit={this.handleSubmit}>
+       <div>
+         <label>
+           Username
+           <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username}/>
+         </label>
+       </div>
+       <div>
+         <label>
+           Password
+           <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password}/>
+         </label>
+       </div>
+       <div>
+         <button type="submit">Log in</button>
+       </div>
+     </form>
+   );
+ }
 }
 
 export default LoginForm;
